18e6f6e7af6121ca2e6b65c10b06302658af7aae
Suriyasiva <ssuriya3098@gmail.com>
2025-05-18 23:27:26 +0530

    [ FIX ] update db config

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,7 +2,16 @@ import { DataSource } from 'typeorm';
 import { ormConfig } from './orm.config';
 import * as dotenv from 'dotenv';
 
-
 dotenv.config();
-const AppDataSource = new DataSource(ormConfig);
+const AppDataSource = new DataSource({
+  ...ormConfig,
+  extra: {
+    family: 4,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  ssl: true,
+  logging: true,
+});
 export default AppDataSource;
